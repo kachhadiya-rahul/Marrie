37ccdb23a340711eed2624b667b9af665d0f50d6
Rahul <rahul@Students-MacBook-Pro.local>
2020-05-22 00:05:24 -0400

    completed crouch and added player prefab

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,8 +7,12 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D PlayerRigidBody;
     Transform PlayerTransform;
     Animator animator;
+
+    PlayerState playerState;
     float distToGround;
+
     float playerRightScale;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,47 +30,62 @@ public class PlayerMovement : MonoBehaviour
     {
         moveVertical = Input.GetAxis("Vertical");
         moveHorizontal = Input.GetAxis("Horizontal");
-        if (moveHorizontal != 0)
-        {
-            animator.SetBool("IsRunning", true);
-
-
-        }
-        else
-        {
-            animator.SetBool("IsRunning", false);
-        }
+        
     }
 
     [Range(0, 10)]
-    public float HorizontalSpeed = 2f;
+    public float HorizontalSpeed = 4f;
+    [Range(0,7)]
+    public float CrouchSpeed = 2f;
     [Range(0, 100)]
     public float HorizontalForce = 50f;
+    [Range(0, 70)]
+    public float CrouchForce = 30f;
     [Range(0, 10)]
     public float JumpForce = 2f;
     private void FixedUpdate()
     {
-        bool shouldIncreaseSpeed = Mathf.Abs(PlayerRigidBody.velocity.x) > HorizontalSpeed && Mathf.Sign(PlayerRigidBody.velocity.x) == Mathf.Sign(moveHorizontal);
+        #region Apply forces to the player for movement
         var shouldJump = moveVertical >= 0.01 && IsGrounded();
         bool shouldCrouch = IsGrounded() && moveVertical <= -0.01;
+        bool shouldNotIncreaseSpeed = Mathf.Abs(PlayerRigidBody.velocity.x) > (shouldCrouch ? CrouchSpeed : HorizontalSpeed) && Mathf.Sign(PlayerRigidBody.velocity.x) == Mathf.Sign(moveHorizontal);
 
-        Vector2 movementMuliplier = new Vector2(!shouldIncreaseSpeed ? HorizontalForce * moveHorizontal : 0, shouldJump ? JumpForce : 0);
+        Vector2 movementMuliplier = new Vector2(!shouldNotIncreaseSpeed ? (shouldCrouch?CrouchForce: HorizontalForce) * moveHorizontal : 0, shouldJump ? JumpForce : 0);
 
         var movementVector = new Vector2(1, 100);
         PlayerRigidBody.AddForce(movementVector * movementMuliplier);
 
+        #endregion
+
+        #region Set animation variables & state
+
+        if (moveHorizontal != 0)
+        {
+            playerState = PlayerState.RUNNING;
+            animator.SetBool("IsRunning", true);
+        }
+        else
+        {
+            playerState = PlayerState.IDLE;
+            animator.SetBool("IsRunning", false);
+        }
         if (shouldJump)
         {
             animator.SetTrigger("jump");
         }
         if (shouldCrouch)
         {
+            playerState = PlayerState.CROUCH;
             animator.SetBool("crouch", true);
         }
         else
         {
             animator.SetBool("crouch", false);
         }
+        animator.SetFloat("land", PlayerRigidBody.velocity.y);
+        #endregion
+
+        #region Change player face direction
         if (PlayerRigidBody.velocity.x > 0.01)
         {
             PlayerRigidBody.transform.localScale = new Vector2(playerRightScale, PlayerRigidBody.transform.localScale.y);
@@ -75,8 +94,9 @@ public class PlayerMovement : MonoBehaviour
         {
             PlayerRigidBody.transform.localScale = new Vector2(-playerRightScale, PlayerRigidBody.transform.localScale.y);
         }
+        #endregion
 
-        animator.SetFloat("land", PlayerRigidBody.velocity.y);
+        
     }
 
     //is player standing on ground
@@ -85,5 +105,12 @@ public class PlayerMovement : MonoBehaviour
         var cast = Physics2D.Raycast(transform.position, Vector2.down, distToGround + 0.4f, LayerMask.GetMask("Platform"));
         var val = cast.collider != null;
         return val;
+    }
+
+    public enum PlayerState
+    {
+        IDLE,
+        RUNNING,
+        CROUCH
     }
 }
