96e393c6f3376856c84ef9ae694139bedf4ef8a6
Rahul <rahul@Students-MacBook-Pro.local>
2020-05-23 13:44:18 -0400

    added gem item

---
+++ Assets/Scripts/GemItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemItem : MonoBehaviour, Icollectible
+{
+    public void CollectItem()
+    {
+        gameObject.SetActive(false);
+        Debug.Log("Gem Item collected");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
